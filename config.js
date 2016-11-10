4691a631aa783eb9bd191c9f964c399a0a38482a
andy.lu <andy.lu@mindspark.com>
2016-11-09 19:41:12 -0500

    bug fix

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
 	hears : 'current conditions for',	
 	rules : {
 		current_conditions_for : {
-			open_query : ''select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'',
+			open_query : 'select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22',
 			end_query : '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
 			result_path : 'query.results.channel.item.condition',
 			result_keys : ['temp','text'],
