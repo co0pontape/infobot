2edfce4da2f5588fb9f763d8827818a3d674f69b
andy.lu <andy.lu@mindspark.com>
2016-11-09 19:38:20 -0500

    externalize queries into config

--- bot.js
+++ bot.js
@@ -25,11 +25,12 @@ if (process.env.PORT) {
 // give the bot something to listen for.
 controller.hears(config.hears, config.type, function(bot,message) {
 
+	var key = config.hears.replace(" ","_");
 	var search = message.text.replace(config.hears,'');
 	var options = config.api;
-	options.path += 'select%20item.condition%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22';
+	options.path += config.rules[key].open_query;
 	options.path += encodeURIComponent(search);
-	options.path +='%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+	options.path += config.rules[key].end_query;
 
 	// do the GET request
 	var reqGet = httpClient.request(options, function(res) {
@@ -44,23 +45,18 @@ controller.hears(config.hears, config.type, function(bot,message) {
 				console.info('\n\nCall completed');			
 			}
 			obj = JSON.parse(d);
-			if(!_.get(obj, config.result_path)) {
+			if(!_.get(obj, config.rules[key].result_path)) {
 				bot.reply(message, message.text + ' ' + config.error_message);	
 			} else {
 				var result = '';
-				for( var i = 0,length = config.result_keys.length; i < length; i++ ) {	
+				for( var i = 0,length = config.rules[key].result_keys.length; i < length; i++ ) {	
 					if(config.debug) {
-						console.info(config.result_keys[i]);
+						console.info(config.rules[key].result_keys[i]);
 					}					
-					if(_.get(obj, config.result_path+'.'+config.result_keys[i])) {
-						if(config.debug) {
-							console.info(config.result_text[config.result_keys[i]].prefix);
-							console.info(_.get(obj, config.result_path+'.'+config.result_keys[i]));
-							console.info(config.result_text[config.result_keys[i]].suffix);
-						}					
-						result += config.result_text[config.result_keys[i]].prefix;
-						result += _.get(obj, config.result_path+'.'+config.result_keys[i]);
-						result += config.result_text[config.result_keys[i]].suffix;
+					if(_.get(obj, config.rules[key].result_path+'.'+config.rules[key].result_keys[i])) {
+						result += config.rules[key].result_text[config.rules[key].result_keys[i]].prefix;
+						result += _.get(obj, config.rules[key].result_path+'.'+config.rules[key].result_keys[i]);
+						result += config.rules[key].result_text[config.rules[key].result_keys[i]].suffix;
 					}
 				}
 				bot.reply(message, message.text + ' is' + result);			
